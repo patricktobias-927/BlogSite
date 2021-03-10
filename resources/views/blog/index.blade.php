754ad061c826b2e2ec4de79ff8c2037c754c1be4
PatrickTobias <patricktobias.business@gmail.com>
2021-03-10 12:43:33 +0800

    Front End Home Page

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,54 @@
+    <div class="container-fluid">
+        <div class="row">
+            <div class="background-image col-sm-12 font-weight-bold h1 text-center text-light mx-auto">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                Welcome Readers.
+                <br>
+                <br>
+                <a class="btn btn-light font-weight-bold" href="/blog">Read More</a>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="container">
+        <div class="row ">
+            <div class="col-2"></div>
+                <div class="col-4">
+                    <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="" srcset="">
+            </div>
+            <div class="col-4  blog-title">
+                <p class="font-weight-bold h1">My First Blog Post</p>
+                <p class="font-italic h5">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                <br>
+                <p class="font-weight-bold h4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <br>
+                <a href="" class="btn btn-primary">Find Out More</a>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-12  text-center text-uppercase">
+                <p class="font-weight-bold h1">Recent Post<p>
+            </div>
+            <div class="col-12  text-center">
+                <p class="font-italic h5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis quasi cum mollitia officiis error. Consequatur laudantium id, dolorum exercitationem cumque eum nam? Blanditiis ipsa ducimus alias similique, atque voluptatem aspernatur?<p>
+            </div>
+        </div>
+    </div>
+
+         
+     
+
+
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,30 @@
+<footer class="bg-info">
+        <div class="container-fluid ">
+            <div class="row">
+                <div class="col-sm-4 bg-dark text-light text-center footer">
+                    <ul class="">
+                        <p class="font-weight-bold">About</p>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque facilis consequatur vitae distinctio, quo quam laudantium, aliquid qui beatae culpa earum. Autem quam ab quasi quia maxime quo atque quibusdam!</li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 bg-dark text-light text-center">
+                    <ul>
+                        <p class="font-weight-bold">Fact</p>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo laborum totam rerum commodi sapiente assumenda aliquam dicta harum molestias fugiat id, placeat, recusandae eveniet dolorem, soluta fugit fuga. Architecto, reprehenderit.</li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 bg-dark text-light">
+                    <ul>
+                        <p class="font-weight-bold">What I Do</p>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ipsum, odio distinctio dolor sapiente suscipit aliquam saepe repudiandae omnis atque ab laborum vero asperiores quidem aperiam nisi quaerat deserunt odit.z</li>
+              
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row d-flex justify-content-center bg-dark text-light">
+                <p>Copyright 2020-2021 Patrick Maine Tobias. All Rights Reserved!</p>
+            </div>
+            
+        </div>
+</footer>
